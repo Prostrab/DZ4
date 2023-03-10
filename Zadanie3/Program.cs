e3b7dffe0171aaee8474a1c58f9239dc7295d473
Prostrab <prostrab@gmail.com>
2023-03-10 13:23:52 +0300

    Задание 3

---
+++ Zadanie3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+int ReadNum (string message)
+{
+Console.WriteLine($"Введите массив из 8 эл-ов {message}");
+string readNum = Console.ReadLine();
+int result = int.Parse(readNum);
+return result;
+}
+
+
+int GenerateArray (int len, int minV , int maxV)
+{
+int [] array = new int [len];
+
+
+}
